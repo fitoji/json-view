27d5ac2edfea7b3591b14230360bb4f520c7f837
fitoji <fitoji@gmail.com>
2024-11-01 12:28:49 +0100

    visor de capacidad de almacenaje invisible cuando menor a 0.3

--- src/components/StorageUsage.jsx
+++ src/components/StorageUsage.jsx
@@ -4,15 +4,20 @@ import { Progress } from "../components/ui/progress"
 
 export default function StorageUsage({ usage }) {
   return (
-    <Card className="text-sm mt-5 bg-[#89eae0] bg-gradient-to-br from-[#89eae0] to-[#f1e8fb]
-    hover:bg-gradient-to-br">
-      <CardHeader>
-        <CardTitle className="text-sm">Uso del Almacenamiento Persistente del Navegador</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Progress value={usage} className="w-full" />
-        <p className="">{usage.toFixed(2)}% usado</p>
-      </CardContent>
-    </Card>
+    <div>
+      {usage > 0.3 &&
+       <Card className="text-sm mt-5 bg-[#89eae0] bg-gradient-to-br from-[#89eae0] to-[#f1e8fb]
+       hover:bg-gradient-to-br">
+         <CardHeader>
+           <CardTitle className="text-sm">Uso del Almacenamiento Persistente del Navegador</CardTitle>
+         </CardHeader>
+         <CardContent>
+           <Progress value={usage} className="w-full" />
+           <p className="">{usage.toFixed(2)}% usado</p>
+         </CardContent>
+       </Card>
+       }
+   
+    </div>
   )
 }
