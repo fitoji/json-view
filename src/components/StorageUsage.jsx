7906573d1b3f87ca41a6242bbe007b6be1f598e9
fitoji <fitoji@gmail.com>
2024-11-01 12:06:08 +0100

    add storageusage, storedfiles, etc...

---
+++ src/components/StorageUsage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card"
+import { Progress } from "../components/ui/progress"
+
+export default function StorageUsage({ usage }) {
+  return (
+    <Card className="text-sm mt-5 bg-[#89eae0] bg-gradient-to-br from-[#89eae0] to-[#f1e8fb]
+    hover:bg-gradient-to-br">
+      <CardHeader>
+        <CardTitle className="text-sm">Uso del Almacenamiento Persistente del Navegador</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Progress value={usage} className="w-full" />
+        <p className="">{usage.toFixed(2)}% usado</p>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/components/StoredFiles.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { SquarePlay, Trash2 } from 'lucide-react'
+import { Separator } from './ui/separator'
+
+export default function StoredFiles({ files, onSelect, onDelete, setTituloOff }) {
+  const fileEntries = Object.entries(files)
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex justify-center md:justify-start">Tests JSON Almacenados</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col">
+        {fileEntries.length === 0 ? (
+          <p>No hay archivos almacenados.</p>
+        ) : (
+          fileEntries.map(([fileName]) => (
+            <div key={fileName} className="flex justify-between flex-col md:flex-row items-center mb-2 border-dotted border-2 border-slate-200">
+              <span className="">{fileName}</span>
+              
+                  <div className="flex flex-row items-center mx-2">
+                    <Button onClick={() => { 
+                      onSelect(fileName); 
+                      setTituloOff(false)
+                      window.scrollTo({ top: 0, behavior: 'smooth' }); 
+                      }} className="rounded-xl bg-sky-500 mr-2 text-base font-bold text-white transition duration-200 hover:bg-sky-400 active:bg-green-100">
+                      <SquarePlay />
+                        <span className="hidden md:inline">
+                        Ver
+                        </span>
+                      </Button>
+                    <Button onClick={() => onDelete(fileName)} variant="destructive">
+                      <Trash2 />
+                      <span className="hidden md:inline">
+                      Eliminar
+                        </span>
+                      
+                      </Button>
+                  </div>
+                  
+              </div>
+            
+          ))
+        )}
+      </CardContent>
+    </Card>
+  )
+}
