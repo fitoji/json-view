e0e9cc1d79751414d8ab036587817b9b5161e2c0
fitoji <fitoji@gmail.com>
2026-04-14 19:09:22 +0200

    actualizacion a vite 8

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react'
 import path from "path"
-
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      // "@": path.resolve(__dirname, "./src"),
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 
