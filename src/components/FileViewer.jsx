207805783679cb20ad73779fcd17db1b8df58c35
fitoji <fitoji@gmail.com>
2026-06-09 14:00:25 +0200

    refactor: remove empty useEffect from FileViewer

--- src/components/FileViewer.jsx
+++ src/components/FileViewer.jsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Test from './quiz/Test'
 
 export default function FileViewer({ content }) {
-    
-    useEffect(() => { // {{ edit_2 }}
-       // console.log(content)
-        
-      }, [content]); // {{ edit_2 }}
     
     return (
           <Test data={content} />
